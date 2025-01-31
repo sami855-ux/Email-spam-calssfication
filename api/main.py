23c146147877e982d6f55f05e67dc9a64eb62401
sami855-ux <samitale56@outlook.com>
2025-01-31 16:31:47 +0500

    Without GUI

--- api/main.py
+++ api/main.py
@@ -3,18 +3,22 @@ from pydantic import BaseModel
 import joblib
 
 # Load model and vectorizer
-model = joblib.load("../models/spam_classifier.pkl")
-vectorizer = joblib.load("../models/vectorizer.pkl")
+model = joblib.load("C:/Users/samit/OneDrive/Desktop/ml22/Email-spam-calssfication/models/spam_classifier.pkl")
+vectorizer = joblib.load("C:/Users/samit/OneDrive/Desktop/ml22/Email-spam-calssfication/models/vectorizer.pkl")
 
 app = FastAPI()
 
 class EmailInput(BaseModel):
     text: str
 
+@app.get("/")  
+def read_root():  
+    return {"message": "Model loaded successfully!"} 
+
 @app.post("/predict/")
 def predict_spam(email: EmailInput):
     transformed_text = vectorizer.transform([email.text])
     prediction = model.predict(transformed_text)[0]
-    return {"prediction": "Spam" if prediction == 1 else "Ham"}
+    return {"prediction": "This message is Spam, Be aware" if prediction == 1 else "This message is not Spam, You can read it"}
 
-# Run with: uvicorn main:app --reload
+# Run with: uvicorn api.main:app --reload
